9368c0ca16931be75584c7354ef56efc9a6aad42
Kajus Buikus <kajusbuikus@gmail.com>
2024-11-03 00:01:35 -0400

    Implemented Items Table

--- main.py
+++ main.py
@@ -90,5 +90,66 @@ def del_trans(trans_id):
     conn.close()
     return jsonify({"message": "Transaction deleted"}), 200
 
+
+# Gets all items
+@app.route("/items", methods=["GET"])
+def get_items():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute('SELECT * FROM \"Items\";')
+    items = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(items)
+
+@app.route("/item/<int:item_id>", methods=["GET"])
+def get_item(item_id):
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute("SELECT * FROM \"Items\" WHERE \"ID\" = %s;", (item_id,))
+    item_data = cur.fetchone()
+    cur.close()
+    conn.close()
+    return jsonify(item_data) if item_data else ('Item not found', 404)
+
+@app.route("/new_item", methods=["POST"])
+def new_item():
+    data = request.get_json()
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO \"Items\" (\"Currency\", \"TransactionID\", \"Quantity\", \"itemName\", \"Amount\", \"VAT\", \"VATPercent\") VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING \"ID\";",
+        (data["Currency"], data["TransactionID"], data["Quantity"], data["itemName"], data["Amount"], data["VAT"], data["vatPercent"])
+    )
+    item_id = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({"message": "Item added"}), 200
+
+@app.route("/edit_item/<int:item_id>", methods=["PUT"])
+def edit_item(item_id):
+    data = request.get_json()
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "UPDATE \"Items\" SET \"Currency\" = %s, \"TransactionID\" = %s, \"Quantity\" = %s, \"itemName\" = %s, \"Amount\" = %s, \"VAT\" = %s, \"VATPercent\" = %s WHERE \"ID\" = %s;",
+        (data["Currency"], data["TransactionID"], data["Quantity"], data["itemName"], data["Amount"], data["VAT"], data["VATPercent"], item_id)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({"message": "Item updated"}), 200
+
+@app.route("/del_item/<int:item_id>", methods=["DELETE"])
+def del_item(item_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM \"Items\" WHERE \"ID\" = %s;", (item_id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({"message": "Item deleted"}), 200
+
 if __name__ == "__main__":
     app.run(debug=True)
